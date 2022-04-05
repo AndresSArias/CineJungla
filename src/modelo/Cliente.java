b391905e08c0edcbb023d458a7de322103166dee
faidercamilo <fctrujilloo@correo.udistrital.edu.co>
2022-04-05 13:17:53 -0500

    avance de modelado y creacion de la base de datos

---
+++ src/modelo/Cliente.java
@@ -0,0 +1,10 @@
+package modelo;
+
+public class Cliente {
+	
+	//Atributos de la clase Cliente
+	private String nombre = " ";
+	private String nombre = " ";
+	private String nombre = " ";
+	private String nombre = " ";
+}
